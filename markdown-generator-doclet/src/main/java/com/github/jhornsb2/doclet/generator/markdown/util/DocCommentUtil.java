4ea7309e3153e77ffcfa9f2d705d62ee5f520100
Joshua Hornsby <joshuahornsby89@gmail.com>
2025-06-10 18:42:28 -0400

    Make it easier to access DocCommentTree

---
+++ markdown-generator-doclet/src/main/java/com/github/jhornsb2/doclet/generator/markdown/util/DocCommentUtil.java
@@ -0,0 +1,28 @@
+package com.github.jhornsb2.doclet.generator.markdown.util;
+
+import java.util.Optional;
+
+import javax.lang.model.element.Element;
+
+import com.sun.source.doctree.DocCommentTree;
+
+import jdk.javadoc.doclet.DocletEnvironment;
+import lombok.NonNull;
+
+public class DocCommentUtil {
+
+    private static DocletEnvironment environment;
+    public static void setEnvironment(DocletEnvironment env) {
+        if (environment != null) {
+            throw new IllegalStateException("DocletEnvironment has already been set.");
+        }
+        environment = env;
+    }
+
+    public static Optional<DocCommentTree> getDocCommentTree(
+		@NonNull Element element
+	) {
+		return Optional.ofNullable(environment.getDocTrees().getDocCommentTree(element));
+	}
+
+}
