e342fd06b53a0495128399c4b08e5e0e4cfd6581
Joshua Hornsby <joshuahornsby89@gmail.com>
2025-07-06 15:46:26 -0400

    Add methods for common functionality

--- markdown-generator-doclet/src/main/java/com/github/jhornsb2/doclet/generator/markdown/processor/IDocletElementProcessor.java
+++ markdown-generator-doclet/src/main/java/com/github/jhornsb2/doclet/generator/markdown/processor/IDocletElementProcessor.java
@@ -1,5 +1,23 @@
 package com.github.jhornsb2.doclet.generator.markdown.processor;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.github.jhornsb2.doclet.generator.markdown.util.OptionUtil;
+import com.google.common.io.Files;
+
 public interface IDocletElementProcessor {
+
+	String getOutputFilepath();
+
+	String toMarkdownString();
+
+	default void processElement() throws IOException {
+		String outputFilepath = this.getOutputFilepath();
+		File outputFile = new File(OptionUtil.getInstance().getDestinationDirectory(), outputFilepath);
+		Files.createParentDirs(outputFile);
+		Files.asCharSink(outputFile, StandardCharsets.UTF_8).write(this.toMarkdownString());
+	}
 
 }
