38144b26987560b9191c2684acfa356b010dd6cd
Joshua Hornsby <12628978+jhornsb2@users.noreply.github.com>
2026-02-28 14:24:44 -0500

    Add constants file for holding standard file names.

---
+++ markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/constants/StandardFileNames.java
@@ -0,0 +1,11 @@
+package com.jhornsb2.doclet.generator.markdown.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StandardFileNames {
+
+  public static final String INDEX_FILE_NAME = "index.md";
+
+}
