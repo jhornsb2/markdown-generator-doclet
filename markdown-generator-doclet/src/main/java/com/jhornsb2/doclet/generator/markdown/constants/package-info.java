0146f387040567e757b5869e175a813c2fb1f70a
Joshua Hornsby <12628978+jhornsb2@users.noreply.github.com>
2026-02-28 14:35:31 -0500

    add package-info

---
+++ markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/constants/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the constants used throughout the Markdown
+ * generator doclet. These constants may include file extensions, directory
+ * names, and other fixed values that are referenced in multiple places
+ * within the codebase.
+ */
+package com.jhornsb2.doclet.generator.markdown.constants;
