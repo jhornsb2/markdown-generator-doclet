2d0cf0dab74643a7d6e647c1fb546abf56ca8e55
Joshua Hornsby <12628978+jhornsb2@users.noreply.github.com>
2026-03-01 16:54:06 -0500

    add unit tests

--- markdown-generator-doclet/src/test/java/com/jhornsb2/doclet/generator/markdown/MarkdownGeneratorDocletTest.java
+++ markdown-generator-doclet/src/test/java/com/jhornsb2/doclet/generator/markdown/MarkdownGeneratorDocletTest.java
@@ -2,10 +2,45 @@ package com.jhornsb2.doclet.generator.markdown;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Proxy;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import jdk.javadoc.doclet.Doclet;
+import jdk.javadoc.doclet.DocletEnvironment;
+import jdk.javadoc.doclet.Reporter;
+
 class MarkdownGeneratorDocletTest {
+
+	private static final Reporter REPORTER = (Reporter) Proxy.newProxyInstance(
+		Reporter.class.getClassLoader(),
+		new Class<?>[] { Reporter.class },
+		(proxy, method, args) -> null
+	);
+
+	private static final DocletEnvironment EMPTY_ENVIRONMENT =
+		(DocletEnvironment) Proxy.newProxyInstance(
+			DocletEnvironment.class.getClassLoader(),
+			new Class<?>[] { DocletEnvironment.class },
+			(proxy, method, args) -> {
+				if ("getIncludedElements".equals(method.getName())) {
+					return Collections.emptySet();
+				}
+				return null;
+			}
+		);
+
+	@BeforeAll
+	static void beforeAll() {
+		MarkdownGeneratorDoclet doclet = new MarkdownGeneratorDoclet();
+		doclet.init(null, REPORTER);
+	}
 
 	@Test
 	void getNameReturnsSimpleClassName() {
@@ -19,5 +54,28 @@ class MarkdownGeneratorDocletTest {
 		MarkdownGeneratorDoclet doclet = new MarkdownGeneratorDoclet();
 
 		assertNotNull(doclet.getSupportedSourceVersion());
+	}
+
+	@Test
+	void getSupportedOptionsIncludesDestinationAndNoTimestamp() {
+		MarkdownGeneratorDoclet doclet = new MarkdownGeneratorDoclet();
+
+		Set<? extends Doclet.Option> options = doclet.getSupportedOptions();
+		Set<String> optionNames = options
+			.stream()
+			.map(Doclet.Option::getNames)
+			.flatMap(List::stream)
+			.collect(java.util.stream.Collectors.toSet());
+
+		assertEquals(Set.of("-d", "-notimestamp"), optionNames);
+	}
+
+	@Test
+	void runReturnsTrueWhenEnvironmentIsEmpty() {
+		MarkdownGeneratorDoclet doclet = new MarkdownGeneratorDoclet();
+
+		boolean result = doclet.run(EMPTY_ENVIRONMENT);
+
+		assertTrue(result);
 	}
 }
