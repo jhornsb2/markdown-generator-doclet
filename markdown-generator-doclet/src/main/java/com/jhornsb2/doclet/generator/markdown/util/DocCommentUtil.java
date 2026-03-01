052baf6a350636fc6efb1ab397a94140d41783e5
Joshua Hornsby <12628978+jhornsb2@users.noreply.github.com>
2026-03-01 16:36:12 -0500

    Javadoc

--- markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/util/DocCommentUtil.java
+++ markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/util/DocCommentUtil.java
@@ -7,11 +7,35 @@ import jdk.javadoc.doclet.DocletEnvironment;
 import lombok.NonNull;
 import lombok.Value;
 
+/**
+ * Utility class for working with Javadoc comments in the context of a
+ * {@link DocletEnvironment}. Provides methods to retrieve and process doc
+ * comments associated with Java elements.
+ */
 @Value
 public class DocCommentUtil {
 
+	/**
+	 * The {@link DocletEnvironment} provides access to the elements being
+	 * processed and their associated doc comments. This environment is
+	 * typically provided by the Javadoc tool when the doclet is executed.
+	 */
 	DocletEnvironment environment;
 
+	/**
+	 * Retrieves the {@link DocCommentTree} for a given {@link Element}, if it
+	 * exists. The {@link DocCommentTree} represents the structured content of
+	 * the Javadoc comment associated with the element, including its
+	 * description and any tags.
+	 *
+	 * @param element The {@link Element} for which to retrieve the doc comment.
+	 *                This is typically a class, method, field, or package
+	 *                {@link Element}.
+	 * @return An {@link Optional} containing the {@link DocCommentTree} if a
+	 *         doc comment is present for the element, or an empty
+	 *         {@link Optional} if no doc comment is associated with the
+	 *         element.
+	 */
 	public Optional<DocCommentTree> getDocCommentTree(
 		@NonNull final Element element
 	) {
