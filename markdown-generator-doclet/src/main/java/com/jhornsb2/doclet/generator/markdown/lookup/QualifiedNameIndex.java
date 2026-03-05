838fa1a6afb5a9de2b77769b5997f28d7635bce0
Joshua Hornsby <12628978+jhornsb2@users.noreply.github.com>
2026-03-05 18:04:05 -0500

    feat: Add QualifiedNameIndex and QualifiedNameIndexEntry classes for mapping qualified names to associated data

---
+++ markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/lookup/QualifiedNameIndex.java
@@ -0,0 +1,113 @@
+package com.jhornsb2.doclet.generator.markdown.lookup;
+
+import com.jhornsb2.doclet.generator.markdown.elements.IElementData;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javax.lang.model.element.Element;
+import lombok.NonNull;
+
+/**
+ * An index that maps qualified names to various pieces of data that are
+ * associated with the element with that qualified name.
+ */
+public class QualifiedNameIndex {
+
+	/**
+	 * Maps qualified names to the path of the documentation file.
+	 */
+	Map<String, String> qualifiedNameToFilePath = new HashMap<>();
+
+	/**
+	 * Maps qualified names to the {@link Element}.
+	 */
+	Map<String, Element> qualifiedNameToElement = new HashMap<>();
+
+	/**
+	 * Maps qualified names to the {@link IElementData}.
+	 */
+	Map<String, IElementData> qualifiedNameToElementData = new HashMap<>();
+
+	/**
+	 * Adds an entry to the index.
+	 *
+	 * @param entry The {@link QualifiedNameIndexEntry} to add.
+	 */
+	public void addEntry(@NonNull final QualifiedNameIndexEntry entry) {
+		this.qualifiedNameToFilePath.put(
+			entry.getQualifiedName(),
+			entry.getFilePath()
+		);
+		this.qualifiedNameToElement.put(
+			entry.getQualifiedName(),
+			entry.getElement()
+		);
+		this.qualifiedNameToElementData.put(
+			entry.getQualifiedName(),
+			entry.getElementData()
+		);
+	}
+
+	/**
+	 * Adds multiple entries to the index.
+	 *
+	 * @param entries The {@link QualifiedNameIndexEntry}s to add.
+	 */
+	public void addEntries(
+		@NonNull final Collection<QualifiedNameIndexEntry> entries
+	) {
+		entries.forEach(this::addEntry);
+	}
+
+	/**
+	 * Gets the file path for the documentation file associated with the element
+	 * with the given qualified name.
+	 *
+	 * @param qualifiedName The qualified name of the element.
+	 * @return The file path of the documentation file.
+	 */
+	public String getFilePath(@NonNull final String qualifiedName) {
+		return this.qualifiedNameToFilePath.get(qualifiedName);
+	}
+
+	/**
+	 * Gets the {@link Element} associated with the element with the given
+	 * qualified name.
+	 *
+	 * @param qualifiedName The qualified name of the element.
+	 * @return The {@link Element}.
+	 */
+	public Element getElement(@NonNull final String qualifiedName) {
+		return this.qualifiedNameToElement.get(qualifiedName);
+	}
+
+	/**
+	 * Gets the {@link IElementData} associated with the element with the given
+	 * qualified name.
+	 *
+	 * @param qualifiedName The qualified name of the element.
+	 * @return The {@link IElementData}.
+	 */
+	public IElementData getElementData(@NonNull final String qualifiedName) {
+		return this.qualifiedNameToElementData.get(qualifiedName);
+	}
+
+	/**
+	 * Gets a {@link QualifiedNameIndexEntry} for the element with the given
+	 * qualified name.
+	 *
+	 * @param qualifiedName The qualified name of the element.
+	 * @return The {@link QualifiedNameIndexEntry} containing the data for the
+	 *         element.
+	 */
+	public QualifiedNameIndexEntry getEntry(
+		@NonNull final String qualifiedName
+	) {
+		return QualifiedNameIndexEntry.builder()
+			.qualifiedName(qualifiedName)
+			.filePath(this.getFilePath(qualifiedName))
+			.element(this.getElement(qualifiedName))
+			.elementData(this.getElementData(qualifiedName))
+			.build();
+	}
+}
---
+++ markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/lookup/QualifiedNameIndexEntry.java
@@ -0,0 +1,41 @@
+package com.jhornsb2.doclet.generator.markdown.lookup;
+
+import com.jhornsb2.doclet.generator.markdown.elements.IElementData;
+import javax.lang.model.element.Element;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * An entry in the {@link QualifiedNameIndex} that contains the qualified name,
+ * file path, element, and element data associated with an element.
+ */
+@Value
+@Builder
+public class QualifiedNameIndexEntry {
+
+	/**
+	 * The qualified name of the element. This is the key that is used to look
+	 * up the file path, element, and element data in the
+	 * {@link QualifiedNameIndex}.
+	 */
+	String qualifiedName;
+
+	/**
+	 * The path of the documentation file associated with the element with the
+	 * qualified name.
+	 */
+	String filePath;
+
+	/**
+	 * The {@link Element} associated with the element with the given qualified
+	 * name.
+	 */
+	Element element;
+
+	/**
+	 * The {@link IElementData} associated with the element with the given
+	 * qualified name.
+	 */
+	IElementData elementData;
+}
