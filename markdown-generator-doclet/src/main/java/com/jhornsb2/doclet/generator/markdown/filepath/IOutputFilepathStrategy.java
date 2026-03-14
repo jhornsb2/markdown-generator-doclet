43a7b5d6e4cedd58461d683b0d586319aa7bf65a
Joshua Hornsby <12628978+jhornsb2@users.noreply.github.com>
2026-03-14 18:04:04 -0400

    feat: Add default method forElement to IOutputFilepathStrategy for handling various element types

--- markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/filepath/IOutputFilepathStrategy.java
+++ markdown-generator-doclet/src/main/java/com/jhornsb2/doclet/generator/markdown/filepath/IOutputFilepathStrategy.java
@@ -1,5 +1,6 @@
 package com.jhornsb2.doclet.generator.markdown.filepath;
 
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ModuleElement;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
@@ -9,6 +10,19 @@ import javax.lang.model.element.TypeElement;
  * types.
  */
 public interface IOutputFilepathStrategy {
+	default String forElement(Element element) {
+		return switch (element.getKind()) {
+			case ANNOTATION_TYPE -> forAnnotationElement((TypeElement) element);
+			case INTERFACE -> forInterfaceElement((TypeElement) element);
+			case CLASS -> forClassElement((TypeElement) element);
+			case ENUM -> forEnumElement((TypeElement) element);
+			case RECORD -> forRecordElement((TypeElement) element);
+			case MODULE -> forModuleElement((ModuleElement) element);
+			case PACKAGE -> forPackageElement((PackageElement) element);
+			default -> throw new IllegalArgumentException("Unsupported element type: " + element.getKind());
+		};
+	}
+
 	String forAnnotationElement(TypeElement annotationElement);
 
 	String forInterfaceElement(TypeElement interfaceElement);
